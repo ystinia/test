6bf6173f31a81790b83daaa51cea9ed263b359df
Ystinia Zalatova (from Futurae PC) <y.zalatova@openipc.org>
2025-11-07 09:43:12 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '',
+        title: 'with love',
         logo: {
           alt: 'OpenIPC Logo',
           src: 'img/logo.svg',
